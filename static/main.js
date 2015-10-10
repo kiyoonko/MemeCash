ddad5e7edcde1ab75abfcb0d7b39773081ef4140
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2015-10-10 16:35:19 -0700

    Chat initiat-able

--- static/main.js
+++ static/main.js
@@ -1,44 +1,43 @@
-function init() {
-    var options = {
-        mode: "sandbox", //for production environment change to "production"
-        client_id: i91BSAwt5Lw,
-        access_token: jP0XCSpdvFA, //valid access token from user authentication
-        invalid_token: function(event) {
-            alert("Access Token expired for session id: " + event.session_id);
-        }
-    };
-
-    Moxtra.init(options);
-}
-
 var client_id = "tmep_FWx9w0";
 var client_secret = "sMXrcuyUul8";
-
 var user = {
     id: "user_1",
     first_name: "Atsushi",
     last_name: "Yamamoto"
 }
-
-
-
 var timestamp = new Date().getTime();
 
 alert(timestamp);
 
-$(document).ready(function() {
-//function getAccessToken(client_id, client_secret, user) {
+function init(accesstoken) {
+    var options = {
+        mode: "sandbox", //for production environment change to "production"
+        client_id: client_id,
+        access_token: accesstoken, //valid access token from user authentication
+        invalid_token: function (event) {
+            alert("Access Token expired for session id: " + event.session_id);
+        }
+    };
+    Moxtra.init(options)
+    Moxtra.chat(options);
+};
+
+$(document).ready(function () {
+    //function getAccessToken(client_id, client_secret, user) {
     $.ajax({
         type: "POST",
         url: "https://apisandbox.moxtra.com/oauth/token",
-        data: "client_id="+client_id+"&client_secret="+client_secret+"&grant_type="+"http://www.moxtra.com/auth_uniqueid"+"&uniqueid="+user.id+"&timestamp="+timestamp+"&firstname="+user.first_name+"&lastname="+user.last_name,
+        data: "client_id=" + client_id + "&client_secret=" + client_secret + "&grant_type=" + "http://www.moxtra.com/auth_uniqueid" + "&uniqueid=" + user.id + "&timestamp=" + timestamp + "&firstname=" + user.first_name + "&lastname=" + user.last_name,
         success: function (data) {
-
             var accesstoken = data.access_token;
             alert(accesstoken);
-            return accesstoken;
-        }});
+            init(accesstoken);
+        }
+    });
 });
+
+
+
 
 var options = {
     binder_id: BINDER_ID,
@@ -48,23 +47,23 @@ var options = {
     iframeheight: "650px",
     autostart_meet: true,
     autostart_note: false,
-    start_chat: function(event) {
+    start_chat: function (event) {
         alert("Chat started session Id: " + event.session_id);
     },
-    start_meet: function(event) {
+    start_meet: function (event) {
         alert("Meet started session key: " + event.session_key + " session id: " + event.session_id);
     },
-    end_meet: function(event) {
+    end_meet: function (event) {
         alert("Meet end event");
     },
-    invite_member: function(event) {
+    invite_member: function (event) {
         alert("Invite member into binder Id: " + event.binder_id);
     },
-    request_note: function(event) {
+    request_note: function (event) {
         alert("Note start request");
     },
-    error: function(event) {
+    error: function (event) {
         alert("Chat error code: " + event.error_code + " error message: " + event.error_message);
     }
 };
-Moxtra.chat(options);
+
