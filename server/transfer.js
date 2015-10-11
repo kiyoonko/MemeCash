661971e2bdc7f3910fbcf1d5d01c50cf82d0dbb8
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2015-10-11 10:49:23 -0700

    burh

--- server/transfer.js
+++ server/transfer.js
@@ -44,6 +44,12 @@ app.post('/user', function (req, res) {
   });
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var server = app.listen(4000, function () {
   var host = server.address().address;
   var port = server.address().port;
