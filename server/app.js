ba6e49453ca7935364a9b97d28e444356f1ff5b2
Kiyoon Ko <kiyoonko@gmail.com>
2015-10-10 18:05:48 -0700

    Took out extended = true

--- server/app.js
+++ server/app.js
@@ -2,9 +2,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended = true;
-}))
+// app.use(bodyParser.urlencoded({
+// 	extended = true;
+// }))
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
