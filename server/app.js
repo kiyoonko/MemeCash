58d7aeaeb798b13d2f7aead6ef1014a63c04873b
Kiyoon Ko <kiyoonko@gmail.com>
2015-10-10 18:02:59 -0700

    Added Creating User Template in server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,28 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended = true;
+}))
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.post('/user', function (req, res) {
+	var postData = {
+  		first_name: req.body.first_name,
+  		last_name: req.body.last_name,
+  		address: {
+    		street_number: req.body.street_number,
+    		street_name: req.body.street_name,
+    		city: req.body.city,
+    		state: req.body.state,
+    		zip: req.body.zip
+  		}
+	} 
+	res.send(postData);
 });
 
 var server = app.listen(3000, function () {
