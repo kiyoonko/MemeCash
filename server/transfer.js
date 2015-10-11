86f7661241bc07a5ef695df7e28c276253070a45
Kiyoon Ko <kiyoonko@gmail.com>
2015-10-11 09:05:34 -0700

    TRANSFER IS DONE
    BACKEND IS OVER

--- server/transfer.js
+++ server/transfer.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var request = require('request');
+var rp = require('request-promise');
 var app = express();
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({
@@ -12,7 +12,36 @@ app.get('/', function (req, res) {
 });
 
 app.post('/user', function (req, res) {
-  
+  var payer_ID = req.body.payer_ID;
+  var postData = { 
+    medium: req.body.medium,
+    payee_id: req.body.payee_ID,
+    amount: req.body.amount
+  }
+  console.log("payer ID", payer_ID);
+  var url = "http://api.reimaginebanking.com/accounts/"+payer_ID+"/transfers?key=e833c6c363ae8cbcad538f4fb79e6492";
+  var options = {
+    method: 'post',
+    body: postData,
+    json: true,
+    uri: url
+  };
+
+
+  rp(options). then(function(createTransaction){
+    console.log("Create Transaction", createTransaction);
+    var options = {
+      method: 'get',
+      uri: url
+    }
+    res.status(200).send({
+        payerID: req.body.payer_ID,
+        payeeID: req.body.payee_ID,
+        money: req.body.amount
+      });
+  }).catch(function(err){
+    console.log("error", err);
+  });
 });
 
 var server = app.listen(3000, function () {
